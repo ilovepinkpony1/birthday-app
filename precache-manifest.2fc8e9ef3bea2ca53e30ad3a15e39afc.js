2824b443a2adbeaa2936f206a87050a7605697ac
Vitalii <ilovepinkpony1@gmail.com>
2019-07-28 20:21:03 +0300

    Updates

---
+++ precache-manifest.2fc8e9ef3bea2ca53e30ad3a15e39afc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d2c91a139270c328a1873868a15b82",
+    "url": "/birthday-app/index.html"
+  },
+  {
+    "revision": "40c4392411082702f02b",
+    "url": "/birthday-app/static/css/main.14f8f6bb.chunk.css"
+  },
+  {
+    "revision": "1fd9481d29262ea3293f",
+    "url": "/birthday-app/static/js/2.d2a50bc9.chunk.js"
+  },
+  {
+    "revision": "40c4392411082702f02b",
+    "url": "/birthday-app/static/js/main.0ad95def.chunk.js"
+  },
+  {
+    "revision": "d254362bb7cde76eee79",
+    "url": "/birthday-app/static/js/runtime~main.44f96cda.js"
+  },
+  {
+    "revision": "26e42e551787bbfc42c6d39e9dff6e9f",
+    "url": "/birthday-app/static/media/VodafoneLt.26e42e55.ttf"
+  },
+  {
+    "revision": "b9a4f47096ca2dfbb0e0657bb8576e0f",
+    "url": "/birthday-app/static/media/VodafoneRg.b9a4f470.ttf"
+  },
+  {
+    "revision": "53aed6b012c25fc2fd2b118dc5d1505c",
+    "url": "/birthday-app/static/media/VodafoneRgBd.53aed6b0.ttf"
+  },
+  {
+    "revision": "fc90f3b1abcaa951ce400842c549b6ef",
+    "url": "/birthday-app/static/media/gift.fc90f3b1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/=pinkpony1.github.io/birthday-app/precache-manifest.a321176f8c1762a5937365ff168e9d7f.js"
+  "/birthday-app/precache-manifest.2fc8e9ef3bea2ca53e30ad3a15e39afc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/=pinkpony1.github.io/birthday-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/birthday-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

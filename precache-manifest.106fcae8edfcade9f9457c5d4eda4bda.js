f955006329dda0da60c9872f86ca0abb510ce95e
Vitalii <ilovepinkpony1@gmail.com>
2019-07-28 20:07:25 +0300

    Updates

---
+++ precache-manifest.106fcae8edfcade9f9457c5d4eda4bda.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cffb32520d350c84e0440d1ab5cfed8",
+    "url": "/=pinkpony1.github.io/borthday-app/index.html"
+  },
+  {
+    "revision": "619ec725a6bb837fb549",
+    "url": "/=pinkpony1.github.io/borthday-app/static/css/main.5592629d.chunk.css"
+  },
+  {
+    "revision": "1fd9481d29262ea3293f",
+    "url": "/=pinkpony1.github.io/borthday-app/static/js/2.d2a50bc9.chunk.js"
+  },
+  {
+    "revision": "619ec725a6bb837fb549",
+    "url": "/=pinkpony1.github.io/borthday-app/static/js/main.0ad95def.chunk.js"
+  },
+  {
+    "revision": "99a290476f2a2dccd9f7",
+    "url": "/=pinkpony1.github.io/borthday-app/static/js/runtime~main.6cd61198.js"
+  },
+  {
+    "revision": "26e42e551787bbfc42c6d39e9dff6e9f",
+    "url": "/=pinkpony1.github.io/borthday-app/static/media/VodafoneLt.26e42e55.ttf"
+  },
+  {
+    "revision": "b9a4f47096ca2dfbb0e0657bb8576e0f",
+    "url": "/=pinkpony1.github.io/borthday-app/static/media/VodafoneRg.b9a4f470.ttf"
+  },
+  {
+    "revision": "53aed6b012c25fc2fd2b118dc5d1505c",
+    "url": "/=pinkpony1.github.io/borthday-app/static/media/VodafoneRgBd.53aed6b0.ttf"
+  },
+  {
+    "revision": "fc90f3b1abcaa951ce400842c549b6ef",
+    "url": "/=pinkpony1.github.io/borthday-app/static/media/gift.fc90f3b1.png"
+  }
+]);
